5cd6f371ac17aa0e38ac23c48bd81197ccee5e37
Dmitri <91905221+Rrturer@users.noreply.github.com>
2022-01-08 22:48:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 module.exports = function toReadable (number) {
-    let one = ['', 'one', 'two', 'three', 'four',
-            'five', 'six', 'seven', 'eight', 'nine',
-            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+    let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
             'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];   
     let ten = ['twenty','thirty', 'forty','fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    let digit = 0;   
+    let n = 0;   
     if (number < 20) return ones[number];   
     if (number < 100) {     
-        digit = number % 10; //remainder     
-        return ten[Math.floor(number/10)-2] + " " + (digit > 0 ? one[digit] : "");   
+        n = number % 10;  
+        return ten[Math.floor(number/10)-2] + " " + (n > 0 ? one[n] : "");   
     }
     if (number < 1000) {    
          return one[Math.floor(number/100)] + " hundred " + (number % 100 > 0 ? toReadable(number % 100) : "");   
